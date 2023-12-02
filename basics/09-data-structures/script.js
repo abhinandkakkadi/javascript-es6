75823825f785c90c0e97fb7c3c6d5d7b612c844a
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-12-02 10:09:36 +0530

    update

--- basics/09-data-structures/script.js
+++ basics/09-data-structures/script.js
@@ -50,6 +50,30 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// The for-of Loop
+const menu2 = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu2) console.log(item);
+
+for (const [i, el] of menu2.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+// console.log([...menu.entries()]);
+
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+///////////////////////////////////////
+
+///////////////////////////////////////
 // Short Circuiting (&& and ||)
 
 console.log("---- OR ----");
